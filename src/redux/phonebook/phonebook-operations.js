2477620dce82a1ee0130fdecb0c1ed01983f3a9a
Elena Semeshyna <81982732+LenaXmi@users.noreply.github.com>
2022-01-25 18:43:24 +0100

    update loading reducer

--- src/redux/phonebook/phonebook-operations.js
+++ src/redux/phonebook/phonebook-operations.js
@@ -66,6 +66,8 @@ export const deleteContact = createAsyncThunk(
 )
 
 
+
+
 //Vanilla async
 
 //  export const fetchContacts = () => async (dispatch) => {
--- src/redux/phonebook/phonebook-reducers.js
+++ src/redux/phonebook/phonebook-reducers.js
@@ -30,7 +30,12 @@ const loading = createReducer(false, {
   [fetchContacts.pending]: () => true,
   [fetchContacts.fulfilled]: () => false,
   [fetchContacts.rejected]:()=>false,
- 
+   [addContact.pending]: () => true,
+  [addContact.fulfilled]: () => false,
+  [addContact.rejected]: () => false,
+    [deleteContact.pending]: () => true,
+  [deleteContact.fulfilled]: () => false,
+  [deleteContact.rejected]:()=>false,
 
 });
 
